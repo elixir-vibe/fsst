e3218c6e4ac0348340b604f04b52e2acc5f43bac
Danila Poyarkov <dev@dannote.net>
2026-05-26 19:45:30 +0300

    Update precompiled NIF checksums for v0.1.2

---
+++ checksum-Elixir.FSST.Native.exs
@@ -0,0 +1,6 @@
+%{
+  "libfsst_nif-v0.1.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:ad739d27cd2dcfe8d00d8ff52536b8a2b0baf3646dd9a66cfa538bc61d474823",
+  "libfsst_nif-v0.1.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:814824d71c282391b97bc3157570e0215a64d0527b64cdf5d6f1a072e3ee4216",
+  "libfsst_nif-v0.1.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:19c3874ad6a179e1c982ec0e44b6de04f0a7dc2897ecb77846edf7612aab3e34",
+  "libfsst_nif-v0.1.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:dbb6ed6c5732f9a5b55e81368a4c61da32b36b21706e72315ed9c88c9bc8715a",
+}
